19546690ad6d1e0f732af0617d2cfa85bdfa9a01
ahliang51 <ahliang51>
2017-06-09 14:37:24 +0800

    changed serverUrl for testing

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,3 +1,8 @@
- export var GlobalVariable = Object.freeze({
-     serverUrl: "http://localhost:3000",
- });
+export var GlobalVariable = Object.freeze({
+
+    //Local Development
+    //  serverUrl: "http://localhost:3000",
+
+    //Deployment
+    serverUrl: "https://wearechime.herokuapp.com",
+});
